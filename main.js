f16c0e6bb6e58d98510c75ada0362cc87ba0f253
Islam hove PC <magomedizlam095111@gmail.com>
2026-05-29 16:45:24 +0300

    сделал записи

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
-const recordButton = document.getElementById('recordButton');
+const form = document.getElementById('appointmentForm');
+const responseBlock = document.getElementById('aiResponse');
 
-recordButton.addEventListener('click', () => {
-    alert('Запись на приём временно недоступна.');
+form.addEventListener('submit', function(event) {
+
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const phone = document.getElementById('phone').value;
+    const message = document.getElementById('message').value;
+
+    responseBlock.style.display = 'block';
+
+    responseBlock.innerHTML = `
+        <strong>AI-ассистент:</strong><br><br>
+
+        Спасибо, ${name}. Ваша заявка успешно принята.
+
+        <br><br>
+
+        Специалист свяжется с вами по номеру:
+        <strong>${phone}</strong>
+
+        <br><br>
+
+        ${
+            message
+                ? `Ваш вопрос: "${message}"`
+                : 'Вы не оставили дополнительный вопрос.'
+        }
+    `;
+
+    form.reset();
 });
